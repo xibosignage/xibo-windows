592bdb6ac151414fb0dbd7dee535d94802785155
Prasadkumarasinghe <kumarasinghe.prasad85@hotmail.com>
2018-07-10 13:03:41 +0530

    Add Option Form to project
    Open Forms using command line argument

--- XiboClient2/XiboClient2/App.xaml.cs
+++ XiboClient2/XiboClient2/App.xaml.cs
@@ -13,5 +13,45 @@ namespace XiboClient2
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            //e.Args is the string[] of command line argruments
+
+            try
+            {
+                // Check for any passed arguments
+                if (e.Args.Length > 0)
+                {
+                    if (e.Args[0].ToString() == "o")
+                    {
+                        RunSettings();
+                    }
+                    else
+                    {
+                        RunClient();
+                    }
+                }
+                else
+                {
+                    RunClient();
+                }
+            }
+            catch
+            { }
+        }
+
+        private static void RunClient()
+        {
+            /// Trace.WriteLine(new LogMessage("Main", "Client Started"), LogType.Info.ToString());
+            MainWindow windowMain = new MainWindow();
+            windowMain.ShowDialog();
+        }
+
+        private static void RunSettings()
+        {
+            // If we are showing the options form, enable visual styles
+            Forms.Options windowMain = new Forms.Options();
+            windowMain.ShowDialog();
+        }
     }
 }
---
+++ XiboClient2/XiboClient2/Forms/Settings/Options.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace XiboClient2.Forms
+{
+    /// <summary>
+    /// Interaction logic for Options.xaml
+    /// </summary>
+    public partial class Options : Window
+    {
+        public string cmsAddress { get; set; }
+        public string key { get; set; }
+        public string localLibrary { get; set; }
+        public string userName { get; set; }
+        public string password { get; set; }
+        public string domain { get; set; }
+        public string displayId { get; set; }
+        public string splashScreen { get; set; }
+
+        public Options()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            save();
+        }
+
+        private void save()
+        {
+            cmsAddress = txtCmsAddress.Text ?? "";
+            key = txtKey.Text ?? "";
+            localLibrary = txtLocalLibrary.Text ?? "";
+            userName = txtUserName.Text ?? "";
+            password = txtPassword.Text ?? "";
+            domain = txtDomain.Text ?? "";
+            displayId = txtDisplayId.Text ?? "";
+            splashScreen = txtSplashScreen.Text ?? "";
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
